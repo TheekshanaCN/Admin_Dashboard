9d3c1c90e6027313ba2161a63425bc5f04762085
TheekshanaCN <theekshanachamodhya@gmail.com>
2025-07-14 13:48:04 +0530

    Auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { register, login } = require('../controllers/auth.controller');
 
-router.get('/test', (req, res) => {
-  res.send('Auth route working ✅');
-});
+router.post('/register', register);
+router.post('/login', login);
 
 module.exports = router;
